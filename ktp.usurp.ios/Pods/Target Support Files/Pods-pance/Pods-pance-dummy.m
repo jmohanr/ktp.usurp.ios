3a6d1c0f7196d4e6fc9c59bbc480505cec1717f1
jagan <jaganmohana.reddy@kaplan.com>
2024-10-14 10:08:30 +0530

    Adding project

---
+++ ktp.usurp.ios/Pods/Target Support Files/Pods-pance/Pods-pance-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pance : NSObject
+@end
+@implementation PodsDummy_Pods_pance
+@end
---
+++ ktp.usurp.ios/ktp.usurp.ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ktp.usurp.ios
+//
+//  Created by Kaplan on 14/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
